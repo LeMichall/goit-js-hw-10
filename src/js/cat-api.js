921b2be3a142c9034407479bf7285aef68fc0a14
LeMichall <120519238+LeMichall@users.noreply.github.com>
2023-06-04 02:44:55 +0200

    fixed

--- src/js/cat-api.js
+++ src/js/cat-api.js
@@ -1,23 +1,26 @@
 const catApi =
   'https://api.thecatapi.com/v1/breeds?api_key=live_lICq8CvALSqDOOGteKLfstNbQ0FFbekBPNTeXFQqaeLv7ofwBQa3owxwAO2DE2EQ';
 const catInfo = 'https://api.thecatapi.com/v1/images/search';
-
+let loading;
+let errorInfo;
 const selectMenu = document.querySelector('.breed-select');
-const loaderMsg = document.querySelector('.loader');
-const errorMsg = document.querySelector('error');
+loading = document.querySelector('.loader');
+errorInfo = document.querySelector('.error');
 const catDesc = document.querySelector('.cat-info');
 
-function loaderHide() {
-  loaderMsg.classList.add('hidden');
+function hideAlert(loader) {
+  loader.classList.add('hidden');
 }
-loaderHide();
+function showAlert(loader) {
+  loader.classList.remove('hidden');
+}
 
 function pingUrl(url) {
   return new Promise((resolve, reject) => {
     fetch(url)
       .then(response => {
         if (!response.ok) {
-          reject(errorMsg);
+          reject(errorInfo);
         } else return response.json();
       })
       .then(data => {
@@ -29,26 +32,43 @@ function pingUrl(url) {
 function fetchBreeds() {
   pingUrl(catApi).then(data => {
     const userChoice = data
-      .map(dataOne => `option value='${dataOne.id}'>${dataOne.name}</option>`)
+      .map(dataOne => `<option value='${dataOne.id}'>${dataOne.name}</option>`)
       .join('');
     selectMenu.insertAdjacentHTML('afterbegin', userChoice);
   });
 }
-fetchBreeds();
 
 function fetchCatByBreed(breedId) {
   const catUrl = `${catInfo}?breed_ids=${breedId}`;
-  pingUrl(catUrl).then(data => {
-    const picture = `<div><img src ="${data[0].url}" class ="cat-pic"></div>`;
-    catDesc.insertAdjacentHTML('afterbegin', picture);
-  });
-  //   .catch(err =>{
-  //     showAlert(errorInfo)
-  //   });
+  pingUrl(catUrl)
+    .then(data => {
+      const picture = `<div><img src ="${data[0].url}" class ="cat-pic"></div>`;
+      catDesc.insertAdjacentHTML('afterbegin', picture);
+    })
+    .catch(err => {
+      showAlert(errorInfo);
+    });
+  const catChoice = `https://api.thecatapi.com/v1/breeds/${breedId}`;
+  pingUrl(catChoice)
+    .then(data => {
+      const catParagraph = `<div class = "cat-txt"><h1>${data.name}</h1><p>${data.description}</p><h2>Temperament</h2>${data.temperament}</p></div>`;
+      catDesc.insertAdjacentHTML('beforeend', catParagraph);
+    })
+    .catch(err => {
+      showAlert(errorInfo);
+    });
 }
 
 function eventHandler(e) {
+  hideAlert(errorInfo);
+  showAlert(loading);
   catDesc.innerHTML = '';
   fetchCatByBreed(e.target.value);
+  setTimeout(() => {
+    hideAlert(loading);
+  }, 500);
 }
+hideAlert(loading);
+hideAlert(errorInfo);
+fetchBreeds();
 selectMenu.addEventListener('change', eventHandler);
